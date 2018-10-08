a6479669096037036bc5674f139dd193cc026e61
Robinson Soares <soares.robinson@gmail.com>
2018-10-07 21:39:39 -0300

    Primeiro Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cursos-client';
+  newItem = {name: '', price: 0, category: ''}
+
+  myList = [
+		{
+			name: 'Angular',
+			price: 140,
+			category: 'Front-End'
+		},
+		{
+			name: 'Express',
+			price: 180,
+			category: 'Back-End'
+		}
+	]
+
+	createItem(){
+		this.myList.push(this.newItem);
+		this.newItem = {name: '', price: 0, category: ''};
+	}
+
+	removeItem(item){
+		var index = this.myList.indexOf(item);
+		this.myList.splice(index, 1);
+	}
 }
